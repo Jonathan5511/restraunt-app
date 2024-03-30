3c14d98e0400f2b4eec45f773db70823fd542386
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-03-30 12:24:54 +0530

    Restruant Summary

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,13 @@
 import CartIcon from "../Cart/CartIcon";
-import Classes from './HeaderCartButton.module.css'
+import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton=props=>{
-    return <button className={Classes.button}>
-        <span className={Classes.icon}>
+    return <button className={classes.button}>
+        <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={Classes.badge}>
+        <span className={classes.badge}>
             3
         </span>
     </button>
